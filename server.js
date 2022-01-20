6447f2b5f2d4051d5329094ee4006046c472e99d
Ani Rogava <anarogava96@gmail.com>
2022-01-20 20:33:52 +0400

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/<crocobet-assignment>"));
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/<crocobet-assignment>/index.html"));
 });
 app.listen(process.env.PORT || 8080);
